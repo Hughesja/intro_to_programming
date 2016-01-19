f012d21a40cda489a978e17a221f34fdf13b5097
Josh Hughes <jhughes26@ymail.com>
2016-01-19 23:31:49 +0000

    Completed variables exercises

---
+++ exercises/variables/name.rb
@@ -0,0 +1,11 @@
+puts "What is your first name?"
+first_name = gets.chomp
+
+puts "What is your last name?"
+last_name = gets.chomp
+
+puts "Welcome " + first_name + " " + last_name
+
+10.times do
+    puts 
+end
